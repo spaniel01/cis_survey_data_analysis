7e3b9bdc3eaf8e312ceee36b66402f34dd82f94f
Dan <danwhereverheis@hotmail.com>
2023-02-28 12:18:26 +0100

    Some major additions to data prep file, esp. renaming vars. Written several functions to generate facet type plots. Continued data analysis of different areas (social, political, ideological), generating various graphics

--- cis_analysis.py
+++ cis_analysis.py
@@ -1,12 +1,14 @@
 # -*- coding: utf-8 -*-
-# -*- coding: utf-8 -*-
 import numpy as np
 import pandas as pd
+import math
 import matplotlib as mplt
 import matplotlib.pyplot as plt
 import seaborn as sns
+mplt.interactive(True)
 
 df = pd.read_pickle("cis_survey_data_revised.pkl")
+dfTypes = pd.DataFrame(df.dtypes)
 
 # Create dict to filter by different party groups as needed 
 partyGroups = {"partidoNacionalTodos" : ['Podemos', 'PP', 'Ciudadanos', 'Ninguno', np.nan, 'PSOE', 'Otro partido', 'VOX', 'Más País', 'IU', 'Más Madrid', 'SUMAR'],
@@ -16,17 +18,255 @@ partyGroups = {"partidoNacionalTodos" : ['Podemos', 'PP', 'Ciudadanos', 'Ninguno
                "regionalTodos" : ['Compromís', 'Andalucía Por Sí', 'Teruel Existe', 'CUP', 'MÉS (PSM-Entesa)', 'ERC', 'Nueva Canarias','CC-PNC', 'BNG', 'PRC', 'JxCat', 'EH Bildu', 'PdeCat', 'EAJ-PNV', 'Geroa Bai', 'UPN']
                } # Sumar, Yolanda, Más País, Errejon
 
+partidoNacionalTodos = pd.CategoricalDtype(categories = ['Podemos', 'Más Madrid', 'Más País', 'SUMAR', 'IU', 'PSOE', 'Ciudadanos', 'PP','VOX', 'Otro partido', 'Ninguno'],
+                                           ordered = True)
+
+# Different variable groups
+df_pol = list(df.iloc[:,list(range(8,59))].columns)
+df_social = list(df.iloc[:,list(np.r_[0:3, 5:8, 59:69])].columns)
+df_survey = list(df.iloc[:,list(np.r_[0:5, 79:-1])].columns)
+df_ideol = ["masCercanoPartido", "autodefIdeol1", "autodefIdeal2", "simpatiaHaciaPartido", "intencionVotoPartido1", "intencionVotoPartido2", "posicionIdeolPropia"]
+df_gastar = [col for col in df if col.startswith('gastar')]
+df_preocup = [col for col in df if col.startswith('preocup')]
+df_liderCononce = [col for col in df if col.startswith('liderConoce')]
+df_liderValoracion = [col for col in df if col.startswith('liderValoracion')]
+df_probEsp = [col for col in df if col.startswith('probEsp')]
+df_probPersonal = [col for col in df if col.startswith('probPersonal')]
+df_situaction_eco = ['valorSitEcoPersonal', 'valorSitEcoEspana']
+
+# Different variable types
+catVars = list(df.select_dtypes("category").columns) 
+objVars = list(df.select_dtypes("object").columns)
+contVars = list(df.select_dtypes("int64").columns) + list(df.select_dtypes("float64").columns)
 
 
-dataRef = pd.DataFrame(df.dtypes).reset_index()
-df.masCercanoPartido.unique()[0]
-df_pol = df.iloc[:, 8:9]
-df_socio = df.iloc[:, np.r_[5:8, 59:69]]
+############################################################################################################################################################  
+sns.set_theme(style = "darkgrid")
+df.columns
+df_social
+
+### Interviewees, sociological profile
+intervieweeCatVars = []
+for catVar in catVars:
+    if catVar in df_social:
+        intervieweeCatVars.append(catVar)
+
+# AC and gender
+#create stacked bar chart
+pd.crosstab(df['ccaa'], df['sexo']).plot(kind='barh', stacked=True)
+plt.tight_layout()
+plt.show();
+
+# Pop of place of origin
+(pd.crosstab(df['ccaa'], df['tamanoMunicipo'], normalize = "index",)*100).plot(kind='barh', stacked=True)
+plt.legend(bbox_to_anchor=(1.02, 1), loc='upper left', borderaxespad=0, title="Bla")
+plt.tight_layout()
+plt.show();
+
+# Age
+sns.displot(data=df, x="edad")
+plt.tight_layout()
+plt.show();
+
+# Social class
+catOrder = list(df.claseSocial.cat.categories)
+catOrder.reverse()
+(df.claseSocial.value_counts(normalize = True)*100)[catOrder].plot(kind = "barh")
+plt.tight_layout()
+plt.show();
+
+# Religion
+(df.religion.value_counts(normalize = True)*100)[df.religion.cat.categories].plot(kind="barh")
+plt.tight_layout()
+plt.show();
+
+# Religion and practicing
+df2=df.loc[df["religion"].isin(["Catholic", "Other religion", "Pract. Catholic"])].copy()
+df2["religion"] = df2.religion.cat.remove_unused_categories()
+g1 =sns.jointplot(data=df2, x="religion", y="practicaRelig", kind="hist")
+g1.ax_joint.set_xticklabels(list(df2.religion.cat.categories), rotation = 45)
+plt.tight_layout()
+plt.show();
+
+### Ideological
 
 ### Ideological affinities
 # masCercanoPartido, autodefIdeol1, autodefIdeal2, simpatiaHaciaPartido,
 # intencionVotoPartido1, intencionVotoPartido2, posicionIdeolPropia
 
+#Ideological position
 sns.displot(x=df.posicionIdeolPropia, kind = "hist", discrete = True)
+plt.tight_layout()
+plt.show();
 
-sns.displot(x=df.masCercanoPartido, kind = "hist", discrete = True)
+#Closest party
+sns.countplot(x=df[df.masCercanoPartido.isin(partyGroups["partidoNacionalTodos"])].masCercanoPartido, 
+              order = df[df.masCercanoPartido.isin(partyGroups["partidoNacionalTodos"])].masCercanoPartido.value_counts().index)
+plt.xticks(rotation=45)
+plt.tight_layout()
+plt.show();
+
+#Simpatia hacia partido
+sns.countplot(x=df[df.simpatiaHaciaPartido.isin(partyGroups["partidoNacionalTodos"])].simpatiaHaciaPartido, 
+              order = df[df.simpatiaHaciaPartido.isin(partyGroups["partidoNacionalTodos"])].simpatiaHaciaPartido.value_counts().index)
+plt.xticks(rotation=45)
+plt.tight_layout()
+plt.show();
+
+#Closest and simpatia
+df2 = df[df.simpatiaHaciaPartido.isin(partyGroups["partidoNacionalTodos"])]
+df2.simpatiaHaciaPartido = df2.simpatiaHaciaPartido.astype(partidoNacionalTodos)
+df2.masCercanoPartido = df.masCercanoPartido.astype(partidoNacionalTodos)
+g1 = sns.jointplot(data = df2, 
+                   x = df2.simpatiaHaciaPartido,
+                   y = df2.masCercanoPartido, 
+                   kind = "hist")
+g1.ax_joint.tick_params(axis='x', rotation=90)
+plt.tight_layout()
+plt.show();
+
+#Self-definition
+g1 =sns.jointplot(data=df, x="autodefIdeolo1", y="autodefIdeolo2", kind="hist")
+g1.ax_joint.set_xticklabels(list(df.autodefIdeolo1.cat.categories), rotation = 90)
+plt.tight_layout()
+plt.show();
+
+# Intencion voto...?
+
+### Political
+if len(df_preocup)/3 > 1:
+      fig, axs = plt.subplots(ncols=3, nrows = math.ceil(len(df_preocup)/3), sharey = True)
+      horizontal = 0
+      vertical = 0
+      for var in df_preocup:
+            sns.countplot(x = var, data=df, ax=axs[vertical, horizontal])
+            ax.tick_params(axis = "x", rotation = 90)
+            horizontal += 1
+            if horizontal > 2:
+                  horizontal = 0
+                  vertical += 1 
+else : 
+      fig, axs = plt.subplots(ncols=3, nrows = 1, sharey = True)
+      horizontal = 0
+      for var in df_preocup:
+            sns.countplot(x = var, data=df, ax=axs[horizontal])
+            horizontal += 1
+for ax in axs.flatten():
+      ax.tick_params(axis = "x", rotation = 90)
+plt.tight_layout()
+plt.show();
+
+def countPlots(varNameList, df, shareXBool = False, figSize=(15, 15), Pad = 1.08, RectTupleLBRT = (0,0,1,1), title = None, Rotation = 90):
+      if len(varNameList)/3 > 1:
+            fig, axs = plt.subplots(ncols=3, nrows = math.ceil(len(varNameList)/3), sharey = True, sharex = shareXBool, figsize=figSize)
+            horizontal = 0
+            vertical = 0
+            for var in varNameList:
+                  sns.countplot(x = var, data=df, ax=axs[vertical, horizontal])
+                  horizontal += 1
+                  if horizontal > 2:
+                        horizontal = 0
+                        vertical += 1 
+      else : 
+            fig, axs = plt.subplots(ncols=len(varNameList), nrows = 1, sharey = True, figsize=figSize)
+            horizontal = 0
+            for var in varNameList:
+                  sns.countplot(x = var, data=df, ax=axs[horizontal])
+                  horizontal += 1
+      axCount = 0
+      for ax in axs.flatten():
+            ax.tick_params(axis = "x", rotation = Rotation)
+            if title is not None:
+                  ax.set_title(varNameList[axCount])
+                  axCount += 1
+      plt.tight_layout(pad = Pad, rect = RectTupleLBRT)
+      plt.show();
+
+def countPlotsNum(varNameList, df, shareXBool = False, figSize=(15, 15), Pad = 1.08, RectTupleLBRT = (0,0,1,1), title = None, Rotation = 90, Binwidth = 1):
+      if len(varNameList)/3 > 1:
+            fig, axs = plt.subplots(ncols=3, nrows = math.ceil(len(varNameList)/3), sharey = True, sharex = shareXBool, figsize=figSize)
+            horizontal = 0
+            vertical = 0
+            for var in varNameList:
+                  sns.histplot(x = var, data=df, discrete = True, ax=axs[vertical, horizontal])
+                  horizontal += 1
+                  if horizontal > 2:
+                        horizontal = 0
+                        vertical += 1 
+      else : 
+            fig, axs = plt.subplots(ncols=len(varNameList), nrows = 1, sharey = True, figsize=figSize)
+            horizontal = 0
+            for var in varNameList:
+                  sns.histplot(x = var, data=df, discrete = True, ax=axs[horizontal])
+                  horizontal += 1
+      axCount = 0
+      for ax in axs.flatten():
+            ax.tick_params(axis = "x", rotation = Rotation)
+            if title is not None:
+                  ax.set_title(varNameList[axCount])
+                  axCount += 1
+      plt.tight_layout(pad = Pad, rect = RectTupleLBRT)
+      plt.show();
+
+# Worries and spending
+countPlots(df_preocup, df)
+countPlots(df_gastar, df, shareXBool = True, Pad = 1.3, RectTupleLBRT = (0,.255,1,1))
+
+# Leadership
+countPlots(df_liderCononce, df, Rotation = 0)
+countPlotsNum(df_liderValoracion, df, Rotation = 0)
+
+# valorar situacion economica
+countPlots(df_situaction_eco, df, title = True)
+sns.jointplot(data=df, x = df_situaction_eco[0], y = df_situaction_eco[1], kind = "hist")
+plt.show();
+
+# Problems in Spain
+df_probEsp_melted = df[df_probEsp].melt()
+df_probEsp_melted.variable = pd.Categorical(df_probEsp_melted.variable, ordered = True, categories = reversed(df_probEsp))
+df_probEsp_melted.value = pd.Categorical(df_probEsp_melted.value, ordered = True, categories = df_probEsp_melted.value.value_counts().index.values)
+sns.displot(data = df_probEsp_melted, y = "value", hue = "variable", discrete = True, kind="hist", multiple = "stack")
+plt.show();
+
+# Personal problems
+df_probPersonal_melted = df[df_probPersonal].melt()
+df_probPersonal_melted.variable = pd.Categorical(df_probPersonal_melted.variable, ordered = True, categories = reversed(df_probPersonal))
+df_probPersonal_melted.value = pd.Categorical(df_probPersonal_melted.value, ordered = True, categories = df_probPersonal_melted.value.value_counts().index.values)
+sns.displot(data = df_probPersonal_melted, y = "value", hue = "variable", discrete = True, kind="hist", multiple = "stack")
+plt.show();
+
+
+df.columns
+
+### Relaitionships
+
+    
+
+# Religion and practicing
+df2=df.loc[df["religion"].isin(["Catholic", "Other religion", "Pract. Catholic"])].copy()
+df2["religion"] = df2.religion.cat.remove_unused_categories()
+g1 =sns.jointplot(data=df2, x="religion", y="practicaRelig", kind="hist")
+g1.ax_joint.set_xticklabels(list(df2.religion.cat.categories), rotation = 45)
+plt.tight_layout()
+plt.show();
+
+df[contVars].corr(method = "spearman")
+
+
+for var in df[catVars].columns:
+      df[var] = df[var].cat.codes
+corrMatrix = df[catVars].corr(method = "kendall")
+sns.heatmap(matrix, annot = True)
+plt.show();
+
+corrMatrix.replace({1.0:np.nan}, inplace = True)
+for col in corrMatrix.columns:
+      for row in corrMatrix.index:
+            if (corrMatrix.loc[row, col] < 0.2) & (corrMatrix.loc[row, col] > -0.2):
+                  corrMatrix.loc[row, col] = np.nan
+corrMatrix.dropna(how = "all")
+corrMatrix = corrMatrix.reset_index().melt(id_vars = "index").dropna(subset="value").sort_values("value", ascending = False)
+corrMatrix[corrMatrix.value.duplicated()]
+
+#
+
--- cis_data_preparation.py
+++ cis_data_preparation.py
@@ -127,7 +127,7 @@ for var in dfTypes.index.values:
             counter += 1
 
 ### Replacing NAN type categories with np.nan for all vars
-allCatVarMerge = {'No tiene criterio':np.nan, 'N.C.':np.nan, 'N.S.':np.nan}
+allCatVarMerge = {'No tiene criterio':np.nan, 'N.C.':np.nan, 'N.S.':np.nan, 'nan':np.nan}
 df.replace(allCatVarMerge, inplace = True)
 
 ### Replacing string values for unique vars
@@ -139,16 +139,97 @@ varsAndValuesToReplace = {
     "paQueElecciones":{'Que los candidatos presenten a los electores sus programas y propuestas para la siguiente legislatura':'que presenten sus programas electorales',
                        'Que los candidatos respondan o rindan cuentas por sus acciones y decisiones en la legislatura que termina':'que rinden cuentas por sus acciones'},
     "posicionIdeolPropia":{'1 Izquierda':1,
-                           '10 Derecha':10}
+                           '10 Derecha':10},
+    "claseSocial":{'No cree en las clases' : np.nan, 'Otras':np.nan,  'No sabe, duda':np.nan,
+    'Clase pobre' : 'Clase baja', 'A los/as de abajo' : 'Clase baja', 'Excluidos/as' : 'Clase baja', 
+    'Clase trabajadora/obrera' : 'Clase media-baja', 'A la gente común' : 'Clase media-baja', 'Proletariado' : 'Clase media-baja'},
+    "religion":{'Católico/a no practicante' : "Catholic", 
+                'Creyente de otra religión' : "Other religion",
+                'Católico/a practicante' : "Pract. Catholic",
+                'Agnóstico/a (no niegan la existencia de Dios pero tampoco la descartan)' : "Agnostic",
+                'Ateo/a (niegan la existencia de Dios)' : "Atheist",
+                'Indiferente, no creyente' : "Indifferent"},
+    "tamanoMunicipo" : {'10.001 a 50.000 habitantes': "10t to 50t", '100.001 a 400.000 habitantes':"100t to 400t",
+                        '2.001 a 10.000 habitantes': "2t to 10t", 'Menos o igual a 2.000 habitantes':"< 2t",
+                        '50.001 a 100.000 habitantes':"50t to 100t" , '400.001 a 1.000.000 habitantes':"400t to 1M",
+                        'Más de 1.000.000 habitantes': "> 1M"},
+    "ccaa" : {'Asturias (Principado de)':'Asturias',
+       'Balears (Illes)' : 'Balears', 'Comunitat Valenciana' : 'Valencia',
+       'Madrid (Comunidad de)' : 'Madrid',
+       'Murcia (Región de)': 'Murcia', 'Navarra (Comunidad Foral de)':'Navarra', 
+       'Rioja (La)': 'La Rioja', 'Ceuta (Ciudad Autónoma de)':'Ceuta',
+       'Melilla (Ciudad Autónoma de)':'Melilla'},
+    "preocupRusiaUcrania" : {'Nada preocupado/a': 'Nada', 'Poco preocupado/a':'Poco', 'Algo preocupado/a':'Algo', 'Bastante preocupado/a':'Bastante', 'Muy preocupado/a':'Muy'},
+    "preocupCambioClimatico" : {'No cree que haya cambio climático':'No existe'},
 }
 for key, value in varsAndValuesToReplace.items():
     df[key].replace(value, inplace = True)
+
+df.ingresosHogar = df.ingresosHogar.str[0:-3]
 
 ### Replacing string values for repeating name vars
 liderValoracion = {'1 Muy mal':1, '10 Muy bien':10}
 for var in df.columns:
     if "liderValoracion" in var:
         df[var].replace({'1 Muy mal':1, '10 Muy bien':10}, inplace = True)
+
+df_gastar = [col for col in df if col.startswith('gastar')]
+for var in df_gastar:
+        df[var] = df[var].str.replace("Gastar ", "")
+        df[var] = df[var].str.replace(" que ahora", "")
+
+preocupVals = {'mal comportamiento de los/as políticos/as' : "comport. político",
+       'Papel de los medios de comunicación y redes: desinformación, manipulación informativa, difusión de bulos' : "medios de comunic.",
+       'problemas políticos en general': "política", 
+       'corrupción y el fraude' : "corrupción",
+       'crisis económica, los problemas de índole económica': "económica" ,
+       'Gobierno y partidos o políticos/as concretos/as' : "actores polít. partic.", 
+       'Aumento de la crispación social, revueltas sociales': "crispación social",
+       'falta de acuerdos, unidad y capacidad de colaboración. Situación e inestabilidad política' : "colab. política",
+       'funcionamiento de la democracia' : "democracia", 
+       'problemas de índole social': "prob. sociales",
+       'subida de tarifas energéticas' : "costes energéticos", 
+       'ocupación de viviendas' : "ocupa. viviendas",
+       'funcionamiento de los servicios públicos' : "servicios públicos", 
+       'Lo que hacen los partidos políticos' : "partidos políticos", 
+       'problemas de la agricultura, ganadería y pesca' : "agricul. etc.",
+       'desigualdades, incluida la de género, las diferencias de clases, la pobreza' : "desigual. y pobreza",
+       'crisis de valores' : "valores",
+       'incertidumbres ante el futuro, la inseguridad y el miedo al futuro' : "el futuro",
+       'Administración de Justicia' : 'admin. de Justicia',
+       'inseguridad ciudadana' : "inseg. ciudadana", 
+       'problemas relacionados con la juventud. Falta de apoyo y oportunidades a los/as jóvenes' : "probl. de los jovenes",
+       'problemas relacionados con la calidad del empleo' : "calidad empleo",
+       'violencia de género' : "viol. de género",
+       'falta de confianza en los/las políticos/as y las instituciones' : "confianza sist. pol.",
+       'peligros para la salud: COVID-19. coronavirus. Falta de recursos suficientes para hacer frente a la pandemia' : "COVID-19",
+       'Guerra de Ucrania y Rusia' : "Ucrania y Rusia",
+       'falta de servicios públicos. recortes' : "servicios públicos", 
+       'subida de impuestos' : 'subida impuestos',
+       'modelo productivo español. falta de inversión en industrias e I+D' : "invers. indust. + I+D",
+       'Poca conciencia ciudadana (falta de civismo, de sentido espíritu cívico)' : "civismo",
+       'Política exterior y relaciones internacionales. Papel de España en el marco internacional': "política exterior",
+       'problemas relacionados con los/as autónomos/as' : "autónomos",
+       'Falta claridad en las informaciones y medidas relacionadas con la COVID-19' : 'info. + medidas COVID-19',
+       'España vaciada, la despoblación' : "España vaciada", 
+       'independencia de Cataluña' : "indep. Cataluña",
+       'Problemas psicológicos (preocupaciones, soledad, tristeza, desamparo, etc.)' : "Prob. psico.",
+       "Desabastecimiento, falta de materias primas y suministros" : "desabastecimiento",
+       "problemas relacionados con la mujer": "probl. de mujeres",
+       "guerras en general" : "guerras",
+       "terrorismo internacional": "terrorismo inter.",
+       "Crisis con Marruecos. Fronteras de Ceuta y Melilla" : "Marruecos, C. y M.",
+       "bajada de impuestos a los/as más ricos/as" : "bajar impuestos a ricos",
+       "Aumento de odio, violencia y agresiones homófobas" : "homófobia",
+       "Catástrofes naturales: el volcán de La Palma" : "volcán de La Palma",
+       "preocupaciones y situaciones personales": "problemas personales", 
+       "Los cambios de hábitos en mi vida cotidiana (no hacer vida normal, etc.)" : "cambios en vida cotidiana" }
+
+for var in df.columns:
+    if "probEsp" in var:  #"probPersonal"
+        for word in ["La ", "El ", "Las ", "Los "]:
+            df[var] = df[var].str.replace(word,"")
+            df[var] = df[var].replace(preocupVals)
 
 ### Replacing different regional names of Podemos to Podemos 
 podemos = ['Podemos', 'Unidas Podemos','En Comú Podem','En Común-Unidas Podemos']
@@ -160,21 +241,31 @@ df.replace(dict.fromkeys(podemos, "Podemos"), inplace = True)
 ordinalVarsUnique = {
     "culpaNoRenovarConsejoGeneral" : ['Ninguno de los dos', 'El PSOE', 'El PP', 'Los dos por igual'],
     "preocupCorona" : ['Nada', 'Poco', 'Regular', 'Bastante', 'Mucho'],
-    "preocupCambioClimatico" : ['No cree que haya cambio climático', 'Nada', 'Regular', 'Bastante', 'Mucho'],
-    "preocupRusiaUcrania" : ['Nada preocupado/a', 'Poco preocupado/a', 'Algo preocupado/a', 'Bastante preocupado/a', 'Muy preocupado/a']
+    "preocupCambioClimatico" : ['No existe', 'Nada', 'Regular', 'Bastante', 'Mucho'],
+    "preocupRusiaUcrania" : ['Nada', 'Poco', 'Algo', 'Bastante', 'Muy'],
+    "ingresosHogar" : df.ingresosHogar.unique()[[0, 5, 1, 4, 3, 2]],
+    "escuela" : ['No, es analfabeto/a', 'No, pero sabe leer y escribir', 'Sí, ha ido a la escuela'],
+    "practicaRelig" : ['Varias veces a la semana', 'Todos los domingos o festivos', 'Dos o tres veces al mes', 'Varias veces al año', 'Casi nunca', 'Nunca'],
+    "tamanoMunicipo": df.tamanoMunicipo.unique()[[3,2,0,4,1, 5, 6]],
+    "claseSocial" : df.claseSocial.unique()[[4,2,1,3,5]],
+    "autodefIdeolo1": ['Comunista', 'Socialista', 'Ecologista', 'Progresista', 'Feminista', 'Socialdemócrata', 'Liberal', 'Demócrata cristiano/a', 'Nacionalista', 'Conservador/a', 'Otra respuesta', 'Apolítico/a'],
+    "autodefIdeolo2": ['Comunista', 'Socialista', 'Ecologista', 'Progresista', 'Feminista', 'Socialdemócrata', 'Liberal', 'Demócrata cristiano/a', 'Nacionalista', 'Conservador/a', 'Otra respuesta', 'Apolítico/a']
     }
-
+df.columns
 for key, values in ordinalVarsUnique.items():
     df[key] = pd.Categorical(df[key], categories = values, ordered = True)
-    
 
 # Repeating vars
 ordinalVarsRep = {
     "valor" : ['Muy mala','Mala', 'Regular', 'Buena',  'Muy buena'],
-    "gastar" : ['Gastar mucho menos que ahora', 'Gastar menos','Gastar lo mismo que ahora', 'Gastar más', 'Gastar mucho más que ahora'],
+    "gastar" : ['mucho menos', 'menos','lo mismo', 'más', 'mucho más'],
     "liderConoce"  : ['No nonoce', 'Conoce'],
     "confianza"  : ['Ninguna confianza', 'Poca confianza', 'Bastante confianza', 'Mucha confianza'],
-    "preperacion"  : ['Poco preparado', 'Muy mal preparado', 'Regular', 'Bastante bien preparado', 'Muy bien preparado']}
+    "preperacion"  : ['Poco preparado', 'Muy mal preparado', 'Regular', 'Bastante bien preparado', 'Muy bien preparado'],
+    "religion" : ['Pract. Catholic', 'Catholic', 'Other religion', 'Atheist' , 'Agnostic', 'Indifferent']
+    }
+
+df.religion.unique()
 
 for varKeyword, categoryOrder in ordinalVarsRep.items():
     for varName in df.columns:
@@ -183,7 +274,7 @@ for varKeyword, categoryOrder in ordinalVarsRep.items():
                                          categories = ordinalVarsRep[varKeyword], 
                                          ordered=True)
             
-### Var type conversion to integer and float
+### Var type conversion to integer, float and cat
 df = df.astype({"edad":"Int64", 
            "probVoto":"Int64", 
            "posicionIdeolPropia":"Int64"})
@@ -195,9 +286,3 @@ for varName in df.columns:
 ### Save changes    
 df.to_pickle("cis_survey_data_revised.pkl")    
 
-
-print(pd.DataFrame(df["maxEstudios"].unique() ))
-    
-
-
-"escuela" : ['No, es analfabeto/a', 'No, pero sabe leer y escribir', 'Sí, ha ido a la escuela']
