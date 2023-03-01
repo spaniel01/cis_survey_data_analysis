96d3605d9205bb3316303905c283a25dc3626448
Dan <danwhereverheis@hotmail.com>
2023-03-01 13:09:17 +0100

    continued generating graphics, started reorganizing different sections

--- cis_analysis.py
+++ cis_analysis.py
@@ -6,22 +6,22 @@ import matplotlib as mplt
 import matplotlib.pyplot as plt
 import seaborn as sns
 mplt.interactive(True)
+sns.set_theme(style = "darkgrid")
 
 df = pd.read_pickle("cis_survey_data_revised.pkl")
 dfTypes = pd.DataFrame(df.dtypes)
 
-# Create dict to filter by different party groups as needed 
+# Party groups
 partyGroups = {"partidoNacionalTodos" : ['Podemos', 'PP', 'Ciudadanos', 'Ninguno', np.nan, 'PSOE', 'Otro partido', 'VOX', 'Más País', 'IU', 'Más Madrid', 'SUMAR'],
                "partidoNacionalVIP" : ['Podemos','PP', 'Ciudadanos', 'Ninguno', np.nan, 'PSOE', 'Otro partido', 'VOX'],
                "catalan" : ['CUP', 'ERC', 'JxCat', 'PdeCat'],
                "regionalVipSocios" : ['ERC', 'PRC', 'PNV', 'EH Bildu'],
                "regionalTodos" : ['Compromís', 'Andalucía Por Sí', 'Teruel Existe', 'CUP', 'MÉS (PSM-Entesa)', 'ERC', 'Nueva Canarias','CC-PNC', 'BNG', 'PRC', 'JxCat', 'EH Bildu', 'PdeCat', 'EAJ-PNV', 'Geroa Bai', 'UPN']
                } # Sumar, Yolanda, Más País, Errejon
-
 partidoNacionalTodos = pd.CategoricalDtype(categories = ['Podemos', 'Más Madrid', 'Más País', 'SUMAR', 'IU', 'PSOE', 'Ciudadanos', 'PP','VOX', 'Otro partido', 'Ninguno'],
                                            ordered = True)
 
-# Different variable groups
+# Variable groups
 df_pol = list(df.iloc[:,list(range(8,59))].columns)
 df_social = list(df.iloc[:,list(np.r_[0:3, 5:8, 59:69])].columns)
 df_survey = list(df.iloc[:,list(np.r_[0:5, 79:-1])].columns)
@@ -33,26 +33,27 @@ df_liderValoracion = [col for col in df if col.startswith('liderValoracion')]
 df_probEsp = [col for col in df if col.startswith('probEsp')]
 df_probPersonal = [col for col in df if col.startswith('probPersonal')]
 df_situaction_eco = ['valorSitEcoPersonal', 'valorSitEcoEspana']
+df_confianza = [col for col in df if col.startswith('confianza')]
+df_preperacion = [col for col in df if col.startswith('preperacion')]
+df_mod = [col for col in df if col.startswith('mod')]
 
-# Different variable types
+# Variable type groups
 catVars = list(df.select_dtypes("category").columns) 
 objVars = list(df.select_dtypes("object").columns)
 contVars = list(df.select_dtypes("int64").columns) + list(df.select_dtypes("float64").columns)
 
 
 ############################################################################################################################################################  
-sns.set_theme(style = "darkgrid")
 df.columns
-df_social
+### NA values
+df_na = df.drop(columns = df_mod).isna().sum()
+df_na = df_na[df_na.iloc[:,] != 0].sort_values()
+df_na.plot(kind = "barh")
+plt.show();
 
-### Interviewees, sociological profile
-intervieweeCatVars = []
-for catVar in catVars:
-    if catVar in df_social:
-        intervieweeCatVars.append(catVar)
+### Social
 
 # AC and gender
-#create stacked bar chart
 pd.crosstab(df['ccaa'], df['sexo']).plot(kind='barh', stacked=True)
 plt.tight_layout()
 plt.show();
@@ -88,50 +89,15 @@ g1.ax_joint.set_xticklabels(list(df2.religion.cat.categories), rotation = 45)
 plt.tight_layout()
 plt.show();
 
-### Ideological
-
-### Ideological affinities
-# masCercanoPartido, autodefIdeol1, autodefIdeal2, simpatiaHaciaPartido,
-# intencionVotoPartido1, intencionVotoPartido2, posicionIdeolPropia
-
-#Ideological position
-sns.displot(x=df.posicionIdeolPropia, kind = "hist", discrete = True)
-plt.tight_layout()
-plt.show();
-
-#Closest party
-sns.countplot(x=df[df.masCercanoPartido.isin(partyGroups["partidoNacionalTodos"])].masCercanoPartido, 
-              order = df[df.masCercanoPartido.isin(partyGroups["partidoNacionalTodos"])].masCercanoPartido.value_counts().index)
-plt.xticks(rotation=45)
-plt.tight_layout()
-plt.show();
-
-#Simpatia hacia partido
-sns.countplot(x=df[df.simpatiaHaciaPartido.isin(partyGroups["partidoNacionalTodos"])].simpatiaHaciaPartido, 
-              order = df[df.simpatiaHaciaPartido.isin(partyGroups["partidoNacionalTodos"])].simpatiaHaciaPartido.value_counts().index)
-plt.xticks(rotation=45)
-plt.tight_layout()
-plt.show();
-
-#Closest and simpatia
-df2 = df[df.simpatiaHaciaPartido.isin(partyGroups["partidoNacionalTodos"])]
-df2.simpatiaHaciaPartido = df2.simpatiaHaciaPartido.astype(partidoNacionalTodos)
-df2.masCercanoPartido = df.masCercanoPartido.astype(partidoNacionalTodos)
-g1 = sns.jointplot(data = df2, 
-                   x = df2.simpatiaHaciaPartido,
-                   y = df2.masCercanoPartido, 
-                   kind = "hist")
-g1.ax_joint.tick_params(axis='x', rotation=90)
-plt.tight_layout()
-plt.show();
-
-#Self-definition
-g1 =sns.jointplot(data=df, x="autodefIdeolo1", y="autodefIdeolo2", kind="hist")
-g1.ax_joint.set_xticklabels(list(df.autodefIdeolo1.cat.categories), rotation = 90)
-plt.tight_layout()
-plt.show();
-
-# Intencion voto...?
+# Add these graphically
+df.columns
+df.escuela.unique()
+df.maxEstudios.unique()
+df.situacionLaboral
+df.situacionProfesional
+df.ocupacion
+df.ingresosHogar
+df.claseSocial
 
 ### Political
 if len(df_preocup)/3 > 1:
@@ -216,7 +182,7 @@ countPlots(df_gastar, df, shareXBool = True, Pad = 1.3, RectTupleLBRT = (0,.255,
 countPlots(df_liderCononce, df, Rotation = 0)
 countPlotsNum(df_liderValoracion, df, Rotation = 0)
 
-# valorar situacion economica
+# Valorar situacion economica
 countPlots(df_situaction_eco, df, title = True)
 sns.jointplot(data=df, x = df_situaction_eco[0], y = df_situaction_eco[1], kind = "hist")
 plt.show();
@@ -235,21 +201,105 @@ df_probPersonal_melted.value = pd.Categorical(df_probPersonal_melted.value, orde
 sns.displot(data = df_probPersonal_melted, y = "value", hue = "variable", discrete = True, kind="hist", multiple = "stack")
 plt.show();
 
+# Confianza
+df_confianza_melted = df[df_confianza].melt()
+df_confianza_melted.value = pd.Categorical(df_confianza_melted.value, ordered = True, categories = df.confianzaEnLiderOposic.cat.categories)
+ax = sns.countplot(data = df_confianza_melted, x = "value",  hue = "variable")
+total = len(df_confianza_melted) - df_confianza_melted.value.isna().sum()
+for p in ax.patches:
+    percentage = f'{100 * p.get_height() / total:.1f}%\n'
+    x = p.get_x() + p.get_width() / 2
+    y = p.get_height()
+    ax.annotate(percentage, (x, y), ha='center', va='center')
+plt.show();
 
-df.columns
+# Preperacion
+df_preperacion_melted = df[df_preperacion].melt()
+df_preperacion_melted.value = pd.Categorical(df_preperacion_melted.value, ordered = True, categories = df.preperacionOposicion.cat.categories)
+ax = sns.countplot(data = df_preperacion_melted, x = "value",  hue = "variable")
+total = len(df_preperacion_melted) - df_preperacion_melted.value.isna().sum()
+for p in ax.patches:
+    percentage = f'{100 * p.get_height() / total:.1f}%\n'
+    x = p.get_x() + p.get_width() / 2
+    y = p.get_height()
+    ax.annotate(percentage, (x, y), ha='center', va='center')
+plt.show();
 
-### Relaitionships
+# Culpa consejo general
+ax = sns.countplot(data = df, x = "culpaNoRenovarConsejoGeneral")
+total = len(df)-df.culpaNoRenovarConsejoGeneral.isna().sum()
+for p in ax.patches:
+    percentage = f'{100 * p.get_height() / total:.1f}%\n'
+    x = p.get_x() + p.get_width()/2
+    y = p.get_height()
+    ax.annotate(percentage, (x, y), ha='center', va='center')
+plt.show();
 
-    
+ax = sns.countplot(data = df, x = "participGenerales19")
+total = len(df)-df.participGenerales19.isna().sum()
+for p in ax.patches:
+    percentage = f'{100 * (p.get_height() / total):.1f}%\n'
+    x = p.get_x() + p.get_width() / 2
+    y = p.get_height()
+    ax.annotate(percentage, (x, y), ha='center', va='center')
+plt.show();
 
-# Religion and practicing
-df2=df.loc[df["religion"].isin(["Catholic", "Other religion", "Pract. Catholic"])].copy()
-df2["religion"] = df2.religion.cat.remove_unused_categories()
-g1 =sns.jointplot(data=df2, x="religion", y="practicaRelig", kind="hist")
-g1.ax_joint.set_xticklabels(list(df2.religion.cat.categories), rotation = 45)
+ax = sns.countplot(data = df, x = "recuerdoVotoPartido19")
+total = len(df)-df.recuerdoVotoPartido19.isna().sum()
+for p in ax.patches:
+    print(p)
+    percentage = f'{100 * p.get_height() / total:.1f}%\n'
+    x = p.get_x() + p.get_width()/2 
+    y = p.get_height()
+    ax.annotate(percentage, (x, y), ha='center', va='center')
+plt.show();
+
+### Ideological
+# masCercanoPartido, autodefIdeol1, autodefIdeal2, simpatiaHaciaPartido,
+# intencionVotoPartido1, intencionVotoPartido2, posicionIdeolPropia
+
+#Ideological position
+sns.displot(x=df.posicionIdeolPropia, kind = "hist", discrete = True)
 plt.tight_layout()
 plt.show();
 
+#Closest party
+sns.countplot(x=df[df.masCercanoPartido.isin(partyGroups["partidoNacionalTodos"])].masCercanoPartido, 
+              order = df[df.masCercanoPartido.isin(partyGroups["partidoNacionalTodos"])].masCercanoPartido.value_counts().index)
+plt.xticks(rotation=45)
+plt.tight_layout()
+plt.show();
+
+#Simpatia hacia partido
+sns.countplot(x=df[df.simpatiaHaciaPartido.isin(partyGroups["partidoNacionalTodos"])].simpatiaHaciaPartido, 
+              order = df[df.simpatiaHaciaPartido.isin(partyGroups["partidoNacionalTodos"])].simpatiaHaciaPartido.value_counts().index)
+plt.xticks(rotation=45)
+plt.tight_layout()
+plt.show();
+
+#Closest and simpatia
+df2 = df[df.simpatiaHaciaPartido.isin(partyGroups["partidoNacionalTodos"])]
+df2.simpatiaHaciaPartido = df2.simpatiaHaciaPartido.astype(partidoNacionalTodos)
+df2.masCercanoPartido = df.masCercanoPartido.astype(partidoNacionalTodos)
+g1 = sns.jointplot(data = df2, 
+                   x = df2.simpatiaHaciaPartido,
+                   y = df2.masCercanoPartido, 
+                   kind = "hist")
+g1.ax_joint.tick_params(axis='x', rotation=90)
+plt.tight_layout()
+plt.show();
+
+#Self-definition
+g1 =sns.jointplot(data=df, x="autodefIdeolo1", y="autodefIdeolo2", kind="hist")
+g1.ax_joint.set_xticklabels(list(df.autodefIdeolo1.cat.categories), rotation = 90)
+plt.tight_layout()
+plt.show();
+
+# Intencion voto...?
+
+
+################################################################################################################################################################ 
+### Relaitionships
 df[contVars].corr(method = "spearman")
 
 
